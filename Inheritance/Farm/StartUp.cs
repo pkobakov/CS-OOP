b57636aaa787eeb2034b3c7a552aa9d39cde91fd
Petar Kobakov <pkobakov@gmail.com>
2023-02-22 17:01:09 +0200

    Hierarchical Inheritance

--- Inheritance/Farm/StartUp.cs
+++ Inheritance/Farm/StartUp.cs
@@ -8,6 +8,10 @@
          Dog dog= new Dog();
             dog.Eat();  
             dog.Bark();
+
+         Cat cat = new Cat();   
+            cat.Eat();
+            cat.Meow();        
         
         }
 
