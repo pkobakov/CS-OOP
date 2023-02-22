b8b79597550b6a926f6019e243128a1e17bf1bef
Petar Kobakov <pkobakov@gmail.com>
2023-02-22 16:35:18 +0200

    Multiple Inheritance

---
+++ Inheritance/Farm/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace Farm
+{
+    public class StartUp
+    {
+
+        public static void Main(string[] args) 
+        { 
+         Dog dog= new Dog();
+            dog.Eat();  
+            dog.Bark();
+        
+        }
+
+    }
+
+
+}
