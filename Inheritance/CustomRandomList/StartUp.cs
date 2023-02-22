cfe35696a6e1e14a83e2b8714f9190de56e10c7d
Petar Kobakov <pkobakov@gmail.com>
2023-02-22 17:49:16 +0200

    Random List

---
+++ Inheritance/CustomRandomList/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace CustomRandomList
+{
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            RandomList list= new RandomList();
+            list.Add("Dimitrichko");
+            list.Add("Pesho");
+            list.Add("Gosho");
+
+
+            Console.WriteLine(list.RandomString()); 
+        }
+    }
+}
