0366a30b1f0b145bbfb4b830bc00fbb546a6b2f2
Petar Kobakov <pkobakov@gmail.com>
2023-02-23 16:31:38 +0200

    Inheritance Lab

---
+++ Inheritance/Lab/StackOfStrings/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace CustomStack
+{
+    public class StartUp
+    {
+        public static void Main (string[] args)
+        {
+
+            StackOfStrings stack = new StackOfStrings ();
+
+            Console.WriteLine(stack.IsEmpty());
+
+            var list = new List<string>() {"C#", "Love", "I"};
+
+            stack.AddRange (list);
+
+            Console.WriteLine(stack.IsEmpty());
+
+            Console.WriteLine(string.Join(" ", stack));
+        }
+    }
+}
