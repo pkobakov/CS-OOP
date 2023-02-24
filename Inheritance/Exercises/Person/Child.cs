aad2fb3254dcba4af3310e4e766876d04b0e373e
Petar Kobakov <pkobakov@gmail.com>
2023-02-24 13:26:19 +0200

    Inheritance Exercise

---
+++ Inheritance/Exercises/Person/Child.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+   public class Child : Person
+   {
+        private int minChildAge = 15;
+        public Child(string name, int age) : base(name, age)
+        {
+            
+        }
+
+
+        public override int Age 
+        { 
+            get => base.Age; 
+            protected set 
+            {
+                if (value <= minChildAge)
+                {
+
+                     base.Age = value; 
+
+                } 
+            }
+            
+        }
+    }
+}
---
+++ Inheritance/Exercises/Person/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name; 
+        private int age;    
+        private const int minAge = 0;
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name 
+        {
+            get { return this.name; }
+            private set { this.name = value; } 
+        }
+        public virtual int Age 
+        {
+            get { return this.age; }
+            protected set 
+            {
+                if (value >= minAge)
+                {
+                 this.age = value; 
+                }
+            }   
+        
+        }
+
+        public override string ToString()
+        {
+            StringBuilder str = new StringBuilder();
+
+            str.Append(String.Format("Name: {0}, Age: {1}", Name, Age));
+
+
+            return str.ToString();
+
+        }
+    }
+}
