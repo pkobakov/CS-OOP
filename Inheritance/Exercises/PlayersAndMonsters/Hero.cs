4c698db212bdef819ffc926af77746d2699ead16
Petar Kobakov <pkobakov@gmail.com>
2023-02-24 17:39:26 +0200

    Hierarchical Inheritance

---
+++ Inheritance/Exercises/PlayersAndMonsters/Hero.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayersAndMonsters
+{
+    public class Hero
+    {
+        public Hero(string username, int level)
+        {
+            this.Username = username;
+            this.Level = level;
+        }
+
+        public string Username { get; set; }
+        public int Level { get; set; }  
+
+        public override string ToString() => $"Type: {this.GetType().Name} Username: {this.Username} Level: {this.Level}";
+    }
+}
--- Inheritance/Exercises/PlayersAndMonsters/StartUp.cs
+++ Inheritance/Exercises/PlayersAndMonsters/StartUp.cs
@@ -1,10 +1,36 @@
-﻿namespace PlayersAndMonsters
+﻿using System;
+using System.Drawing;
+using System.Reflection;
+using System.Runtime.Intrinsics.Arm;
+
+namespace PlayersAndMonsters
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
+            string username = System.Console.ReadLine();
+            int level = int.Parse(System.Console.ReadLine());
 
+            Hero hero = new Hero(username, level);
+            Elf elf = new Elf(username, level);
+            Wizard wizard = new Wizard(username, level);
+            Knight knight = new Knight(username, level);
+            MuseElf museElf = new MuseElf(username, level);
+            DarkWizard darkWizard = new DarkWizard(username, level);
+            DarkKnight darkKnight = new DarkKnight(username, level);
+            SoulMaster soulMaster = new SoulMaster(username, level);
+            BladeKnight bladeKnight = new BladeKnight(username, level);
+
+            Console.WriteLine(hero);
+            Console.WriteLine(elf);
+            Console.WriteLine(wizard);
+            Console.WriteLine(knight);
+            Console.WriteLine(museElf);
+            Console.WriteLine(darkWizard);
+            Console.WriteLine(darkKnight);
+            Console.WriteLine(soulMaster);
+            Console.WriteLine(bladeKnight);
         }
     }
 }
