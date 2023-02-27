3918cb3868d9fab5770f360ad9f03c2afab3801a
Petar Kobakov <pkobakov@gmail.com>
2023-02-27 17:10:34 +0200

    Mutable vs Immutable Objects

--- Encapsulation/Lab/Encapsulation_Lab/PersonsInfo/Engine.cs
+++ Encapsulation/Lab/Encapsulation_Lab/PersonsInfo/Engine.cs
@@ -15,6 +15,7 @@ namespace PersonsInfo
 
             var lines = int.Parse(Console.ReadLine());
             var persons = new List<Person>();
+            Team team = new Team("SoftUni");
 
             for (int i = 0; i < lines; i++)
                {
@@ -32,10 +33,15 @@ namespace PersonsInfo
                 }
             }
 
+            foreach (var person in persons)
+            { 
+               team.AddPlayer(person);
+            
+            }
 
-            var parcentage = decimal.Parse(Console.ReadLine());
-            persons.ForEach(p => p.IncreaseSalary(parcentage));
-            persons.ForEach(p => Console.WriteLine(p.ToString()));
+            Console.WriteLine(team.ToString());
+
+
 
 
 
---
+++ Encapsulation/Lab/Encapsulation_Lab/PersonsInfo/Team.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private readonly List<Person> firstTeam;
+        private readonly List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            this.name = name;
+            this.firstTeam = new List<Person>();
+            this.reserveTeam = new List<Person>();
+        }
+
+
+
+        public IReadOnlyList<Person> FirstTeam { get => firstTeam.AsReadOnly();}
+        public IReadOnlyList<Person> ReserveTeam { get => reserveTeam.AsReadOnly();}
+
+
+        public void AddPlayer(Person person) 
+        {
+            if (person.Age < 40)
+            {
+                this.firstTeam.Add(person);
+            } 
+
+            else
+            {
+              this.reserveTeam.Add(person);
+
+            }
+        
+        }
+
+        public override string ToString()
+        {
+            return $"First team has {FirstTeam.Count} players.{Environment.NewLine}Reserve team has {ReserveTeam.Count} players. ";
+        }
+
+    }
+}
