569f97b0f8413c916d31d5cd1ee6a830e51e97d6
Petar Kobakov <pkobakov@gmail.com>
2023-02-28 16:30:14 +0200

    Property encapsulation with custom validation

---
+++ Encapsulation/Exercise/Box/Engine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Box
+{
+    public static class Engine
+    {
+        public static void Run() 
+        {
+            try
+            {
+                double length = double.Parse(Console.ReadLine());
+                double width = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+
+                Box box = new Box(length, width, height);
+
+
+                Console.WriteLine(box.ToString());
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
---
+++ Encapsulation/Exercise/Box/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace Box
+{
+	using System;
+    using System.Runtime.Serialization;
+
+    public class StartUp
+	{
+		public static void Main(string[] args)
+		{
+			Engine.Run();
+		}
+	}
+}
