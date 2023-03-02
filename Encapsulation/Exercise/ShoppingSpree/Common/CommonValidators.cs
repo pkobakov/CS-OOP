316f695f0cbcc2cea68b9b9414a7e62f933897b9
Petar Kobakov <pkobakov@gmail.com>
2023-03-02 17:23:07 +0200

    Single Encapsulation

---
+++ Encapsulation/Exercise/ShoppingSpree/Common/CommonValidators.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingSpree.Common
+{
+    public  class CommonValidators
+    {
+        private const decimal MoneyMinValue = 0;
+        public static void ValidateName(string value)
+        {
+            if (String.IsNullOrEmpty(value) || String.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Name cannot be empty");
+            }
+        }
+        public static void ValidateSum(decimal value)
+        {
+            if (value < MoneyMinValue)
+            {
+                throw new ArgumentException("Money cannot be negative");
+            }
+        }
+    }
+}
