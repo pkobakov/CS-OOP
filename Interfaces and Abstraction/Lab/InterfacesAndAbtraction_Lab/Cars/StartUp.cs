ba509808082b7b8f757c0d4c7707d18b15c7fa22
Petar Kobakov <pkobakov@gmail.com>
2023-03-06 15:09:27 +0200

    Interface Example

---
+++ Interfaces and Abstraction/Lab/InterfacesAndAbtraction_Lab/Cars/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace Cars
+{
+    using Cars.Contracts;
+    using Cars.Models;
+    using System;
+    public class StartUp
+    {
+        public static void Main(string[] args)
+		{
+            ICar seat = new Seat("Leon", "Grey");
+            ICar tesla = new Tesla("Model 3", "Red", 2);
+
+            Console.WriteLine(seat.ToString());
+            Console.WriteLine(tesla.ToString());
+
+        }
+    }
+}
