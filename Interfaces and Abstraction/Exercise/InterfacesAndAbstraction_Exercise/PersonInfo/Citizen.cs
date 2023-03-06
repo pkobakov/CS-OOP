f07a628edf293931fe109b7e08aff25030a47b9a
Petar Kobakov <pkobakov@gmail.com>
2023-03-06 16:28:21 +0200

    Multiple Implementation

--- Interfaces and Abstraction/Exercise/InterfacesAndAbstraction_Exercise/PersonInfo/Citizen.cs
+++ Interfaces and Abstraction/Exercise/InterfacesAndAbstraction_Exercise/PersonInfo/Citizen.cs
@@ -1,22 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PersonInfo
 {
-    public class Citizen : IPerson
+    public class Citizen : IPerson, IIdentifiable, IBirthable
     {
         string name;
         int age;
-        public Citizen(string name, int age)
+        string id;
+        string birthdate;
+        public Citizen(string name, int age, string id, string birthdate)
         {
             this.Name = name;
             this.Age = age;
-        }
-        public string Name { get; set; }
+            this.Id = id;
+            this.Birthdate = birthdate;
 
-        public int Age { get; set; }
+        }
+        public string Name { get => name; private set { name = value; } }
+
+        public int Age { get => age; private set { age = value; } }
+
+        public string Id { get => id; private set { id = value; } }
+
+        public string Birthdate { get => birthdate; private set { birthdate = value; } }
+
+
     }
 }
