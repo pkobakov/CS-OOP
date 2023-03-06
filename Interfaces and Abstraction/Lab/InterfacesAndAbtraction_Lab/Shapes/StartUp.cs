fe9f5a96dd1a752b427a5b8fabf0b80f28a5acfc
Petar Kobakov <pkobakov@gmail.com>
2023-03-06 14:18:29 +0200

    Interface Example

---
+++ Interfaces and Abstraction/Lab/InterfacesAndAbtraction_Lab/Shapes/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace Shapes
+{
+    using Shapes.Contracts;
+    using System;
+    public class StartUp
+    {
+        public static void Main(string[] args)
+		{
+            var radius = int.Parse(Console.ReadLine());
+            IDrawable circle = new Circle(radius);
+
+            var width = int.Parse(Console.ReadLine());
+            var height = int.Parse(Console.ReadLine());
+            IDrawable rect = new Rectangle(width, height);
+
+            circle.Draw();
+            rect.Draw();
+
+        }
+    }
+}
