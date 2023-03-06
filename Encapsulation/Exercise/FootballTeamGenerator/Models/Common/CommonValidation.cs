d76ef8e85c83238b1aa237ef1539d8c5456f2076
Petar Kobakov <pkobakov@gmail.com>
2023-03-06 12:40:28 +0200

    Encapsulation and Property Validation

---
+++ Encapsulation/Exercise/FootballTeamGenerator/Models/Common/CommonValidation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballTeamGenerator.Models.Common
+{
+    public class CommonValidation
+    {
+        private const int MinStatPoints = 0;
+        private const int MaxStatPoints = 100;
+        public static void ValidateName(string value)
+        {
+            if (String.IsNullOrEmpty(value) || String.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("A name should not be empty.");
+            }
+        }
+
+        public static void ValidateStatPoints(int value, string stat)
+        {
+            if (value < MinStatPoints || value > MaxStatPoints)
+            {
+                throw new ArgumentException($"{stat} should be between {MinStatPoints} and {MaxStatPoints}.");
+            }
+        }
+    }
+}
