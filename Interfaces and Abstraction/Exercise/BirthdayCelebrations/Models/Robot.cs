c433c6973dcdd723b6967a41a12058ab92b5bdad
Petar Kobakov <pkobakov@gmail.com>
2023-03-07 23:48:42 +0200

    Problem Solution

---
+++ Interfaces and Abstraction/Exercise/BirthdayCelebrations/Models/Robot.cs
@@ -0,0 +1,20 @@
+﻿using BirthdayCelebrations.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthdayCelebrations.Models
+{
+    public class Robot : IIdentifiable
+    {
+        public Robot(string model, string id)
+        {
+            this.Model = model;
+            this.ID = id;
+        }
+        public string Model { get; set; }
+        public string ID { get; private set; }
+    }
+}
