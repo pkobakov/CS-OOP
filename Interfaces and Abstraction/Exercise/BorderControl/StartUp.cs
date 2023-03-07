d75cc309fd0c1fd23c9f228152de92b73a31ae6d
Petar Kobakov <pkobakov@gmail.com>
2023-03-07 23:49:13 +0200

    Problem Solution

---
+++ Interfaces and Abstraction/Exercise/BorderControl/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace BorderControl
+{
+    using BorderControl.Contracts;
+    using BorderControl.Core;
+    using BorderControl.Models;
+    using System;
+	public class StartUp
+	{
+		public static void Main(string[] args)
+		{
+			var engine = new Engine();
+			engine.Run();
+
+        }
+	}
+}
