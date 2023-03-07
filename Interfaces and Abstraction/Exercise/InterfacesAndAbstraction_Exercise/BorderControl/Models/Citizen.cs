293477c9b5a2fd3e0ae00419c91f298db107becb
Petar Kobakov <pkobakov@gmail.com>
2023-03-07 17:51:31 +0200

    Interface Implementation

---
+++ Interfaces and Abstraction/Exercise/InterfacesAndAbstraction_Exercise/BorderControl/Models/Citizen.cs
@@ -0,0 +1,26 @@
+﻿using BorderControl.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BorderControl.Models
+{
+    public class Citizen : IIdentifiable
+    {
+        private string name;
+        private int age;
+        private string id;
+
+        public Citizen(string name, int age, string id) 
+        { 
+           this.Name = name;
+            this.Age = age;
+            this.Id = id;
+        }
+        public string Name { get => name; private set { name = value; } }
+        public int Age { get => age; private set { age = value; } }
+        public string Id { get => id; private set { id = value; } }
+    }
+}
