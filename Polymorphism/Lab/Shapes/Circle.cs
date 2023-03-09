ea08f9dca727ee203485aa48696d0f0ef5606546
Petar Kobakov <pkobakov@gmail.com>
2023-03-09 15:25:18 +0200

    Polymorphism and overriding

---
+++ Polymorphism/Lab/Shapes/Circle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+
+        public double Radius { get; private set; }
+        public override double CalculateArea()
+        {
+            return Math.PI * Math.Pow(this.Radius, 2);
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2*(Math.PI*this.Radius);
+        }
+
+        public override string Draw()
+        {
+            return base.Draw();
+        }
+
+    }
+}
