b68486ea217cfd6bca67bc4daf1500ef48820b9f
Petar Kobakov <pkobakov@gmail.com>
2023-03-16 16:18:31 +0200

    Add Solution

--- Reflection and Attributes/Lab/Stealer/Spy.cs
+++ Reflection and Attributes/Lab/Stealer/Spy.cs
@@ -29,9 +29,45 @@ namespace Stealer
                 sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
                    
             }
-
-            
             return sb.ToString();
+        }
+
+        public string AnalyzeAccessModifiers(string className) 
+        {
+            Type classType;
+            string classFullName = string.Empty;
+            Assembly myAssembly = Assembly.GetExecutingAssembly();
+            var types = myAssembly.GetTypes();
+            foreach (var type in types.Where(t => t.Name == className)) 
+            {
+                classFullName = type.FullName;
+            }
+
+           
+
+            StringBuilder sb = new StringBuilder();
+            classType = Type.GetType(classFullName);
+            FieldInfo [] nonPrivateFields = classType.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static);
+            MethodInfo[] nonPublicMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+            MethodInfo[] publicMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.Public);
+
+            foreach (var field in nonPrivateFields )
+            {
+                sb.AppendLine($"{field.Name} must be private!");
+            }
+
+            foreach (var method in nonPublicMethods.Where(m => m.Name.StartsWith("get")))
+            {
+
+                sb.AppendLine($"{method.Name} have to be public!"); 
+            }
+
+            foreach (var method in publicMethods.Where(m => m.Name.StartsWith("set")))
+            {
+                sb.AppendLine($"{method.Name} have to be private!");
+            }
+
+            return sb.ToString().Trim();
         }
     }
 }
--- Reflection and Attributes/Lab/Stealer/StartUp.cs
+++ Reflection and Attributes/Lab/Stealer/StartUp.cs
@@ -8,10 +8,14 @@
 		public static void Main(string[] args)
 		{
 			Spy spy = new Spy();
-			string result = spy.StealFieldInfo("Stealer.Hacker", new[] { "username", "password" });
+			//string result = spy.StealFieldInfo("Stealer.Hacker", new[] { "username", "password" });
+			string result = spy.AnalyzeAccessModifiers("Hacker");
 			Console.WriteLine(result);
 
+			
+          
 
-		}
+
+        }
 	}
 }
