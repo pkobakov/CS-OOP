3c5c7f6fb3e766e4da0e987a7d58b35f60955038
Petar Kobakov <pkobakov@gmail.com>
2023-03-20 17:05:20 +0200

    Add Solutions

---
+++ Unit Testing/Lab/Skeleton.Tests/DummyTests.cs
@@ -0,0 +1,75 @@
+﻿using NUnit.Framework;
+
+namespace Skeleton.Tests
+{
+    [TestFixture]
+    public class DummyTests
+    {
+        [Test]
+        public void Test_DummyLosesHealthAfterAttack() 
+        {
+            //Arrange
+            Dummy dummy = new Dummy(10, 10);
+            Axe axe = new Axe(10, 10);
+
+            //Act
+
+            dummy.TakeAttack(axe.AttackPoints);
+
+            //Assert
+
+            Assert.That(dummy.Health, Is.EqualTo(0), "Dummy doesn't loose Health after attack");
+        }
+
+        [Test]
+        public void Test_DeadDummyThrowsAnExceptionIfAttacked() 
+        {
+
+            //Arrange
+
+            Dummy dummy = new Dummy(0,10);
+            Axe axe = new Axe(10, 10);
+
+            //Assert
+            Assert.That(() => dummy.TakeAttack(axe.AttackPoints), Throws
+                                                                       .InvalidOperationException
+                                                                       .With
+                                                                       .Message
+                                                                       .EqualTo("Dummy is dead."));
+        }
+
+        [Test]
+        public void Test_DeadDummyCanGiveXP() 
+        {
+            //Arrange
+            Dummy dummy = new Dummy(10, 10);
+            Axe axe = new Axe(10,10);
+            
+            //Act
+
+            dummy.TakeAttack(axe.AttackPoints);
+
+            //Assert
+
+            Assert.That(dummy.GiveExperience, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void Test_AliveDummyCannotGiveXP() 
+        {
+            //Arrange
+            Dummy dummy = new Dummy(20, 20);
+            Axe axe = new Axe(10, 10);
+
+            //Act
+
+            dummy.TakeAttack(axe.AttackPoints);
+
+            //Assert
+
+            Assert.That(dummy.GiveExperience,Throws.InvalidOperationException.With.Message.EqualTo("Target is not dead."));
+        }
+    }
+
+
+}
