4c151aae40060df97688aaa05a06a4d3a9a26278
Petar Kobakov <pkobakov@gmail.com>
2023-03-31 11:23:11 +0300

    Add Structure

---
+++ Exam Prep/12 DEC 2020/Bakery/Bakery/Models/Drinks/Models/Drink.cs
@@ -0,0 +1,78 @@
+﻿using Bakery.Models.Drinks.Contracts;
+using Bakery.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bakery.Models.Drinks.Models
+{
+    public abstract class Drink : IDrink
+    {
+        private string name;
+        private int portion;
+        private decimal price;
+        private string brand;
+
+        public Drink(string name, int portion, decimal price, string brand)
+        {
+            this.Name = name;
+            this.Portion = portion;
+            this.Price = price;
+            this.Brand = brand;
+        }
+        public string Name
+        {
+            get { return name; }
+            private set 
+            {
+                if (string.IsNullOrWhiteSpace(value)) 
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidName);
+                }
+                this.name = value; 
+            }
+        }
+
+        public int Portion
+        {
+            get { return portion; }
+            private set 
+            { 
+                if (value <= 0) 
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidPortion);
+                }
+                this.portion = value; 
+            }
+        }
+
+        public decimal Price
+        {
+            get { return price; }
+            private set
+            {
+                if (value <= 0) 
+                { 
+                    throw new ArgumentException(ExceptionMessages.InvalidPrice);    
+                }
+                this.price = value;
+            }
+        }
+
+        public string Brand 
+        {
+            get { return brand; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value)) 
+                { 
+                    throw new ArgumentException(ExceptionMessages.InvalidBrand);    
+                }
+                this.brand = value;
+            }
+        }
+
+        public override string ToString()
+         => $"{this.Name} {this.Brand} - {this.Portion}ml - {this.Price:f2}lv";
+    }
+}
---
+++ Exam Prep/12 DEC 2020/Bakery/Bakery/StartUp.cs
@@ -0,0 +1,19 @@
+﻿namespace Bakery
+{
+    using System;
+    using Bakery.Core;
+    using Bakery.Models.BakedFoods.Models;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            //Don't forget to comment out the commented code lines in the Engine class!
+            //var engine = new Engine();
+
+            //engine.Run();
+
+         
+        }
+    }
+}
