cc629cb4c8a6378b471fb4ed00fa1c08aefa2bd2
Petar Kobakov <pkobakov@gmail.com>
2023-03-31 14:43:27 +0300

    Add Business Logic

--- Exam Prep/12 DEC 2020/Bakery/Bakery/StartUp.cs
+++ Exam Prep/12 DEC 2020/Bakery/Bakery/StartUp.cs
@@ -9,11 +9,10 @@
         public static void Main(string[] args)
         {
             //Don't forget to comment out the commented code lines in the Engine class!
-            //var engine = new Engine();
+            var engine = new Engine();
+            engine.Run();
 
-            //engine.Run();
 
-         
         }
     }
 }
