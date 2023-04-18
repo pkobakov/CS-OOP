b55d4333e647914f7f082b19cc1d0ad1533aecf6
Petar Kobakov <pkobakov@gmail.com>
2023-04-18 17:11:54 +0300

    Add Structure

---
+++ Exam Prep/18 APR 2021/Easter/Easter/Models/Workshops/Workshop.cs
@@ -0,0 +1,30 @@
+﻿using Easter.Models.Bunnies.Contracts;
+using Easter.Models.Eggs.Contracts;
+using Easter.Models.Workshops.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Easter.Models.Workshops
+{
+    internal class Workshop : IWorkshop
+    {
+        public void Color(IEgg egg, IBunny bunny)
+        {
+           while (!egg.IsDone() && 
+                   bunny.Dyes.Any( d => !d.IsFinished()) &&
+                   bunny.Energy > 0) 
+           {
+            
+                var dye = bunny.Dyes.FirstOrDefault( d => !d.IsFinished());
+                dye.Use();
+                egg.GetColored();
+                bunny.Work();
+
+            
+           }
+
+        }
+    }
+}
