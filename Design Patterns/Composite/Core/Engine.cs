314500f5cc790827f58e5cd2a4c09a8fc3702351
Petar Kobakov <pkobakov@gmail.com>
2023-07-04 18:05:20 +0300

    Added New Solution

---
+++ Design Patterns/Composite/Core/Engine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Composite.Core.Contracts;
+using Composite.Models;
+
+namespace Composite.Core
+{
+    public class Engine : IEngine
+    {
+        public void Run()
+        {
+            SingleGift phone = new SingleGift("IPhone", 253);
+            SingleGift watch = new SingleGift("Swacth", 344);
+
+
+            CompositeGift shop = new CompositeGift("Boutique", 0);
+
+            shop.Add(phone);
+            shop.Add(watch);
+            Console.WriteLine(shop.CalculatePrice());
+        }
+    }
+}
---
+++ Design Patterns/Composite/Models/Contracts/IGiftOperations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite.Models.Contracts
+{
+    public interface IGiftOperations
+    {
+        void Add(GiftBase gift);
+        void Remove(GiftBase gift);
+    }
+}
