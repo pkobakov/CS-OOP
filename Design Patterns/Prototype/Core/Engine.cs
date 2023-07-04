eb7db8789f3d359f355831eaf6aa846805899d94
Petar Kobakov <pkobakov@gmail.com>
2023-07-04 17:19:08 +0300

    Added New Solution

---
+++ Design Patterns/Prototype/Core/Engine.cs
@@ -0,0 +1,36 @@
+﻿using Prototype.Core.Contracts;
+using Prototype.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype.Core
+{
+    public class Engine : IEngine
+    {
+       
+        public Engine()
+        {
+            
+        }
+        public void Run()
+        {
+            SandwichMenu sandwichMenu = new SandwichMenu();
+
+            sandwichMenu["BLT"] = new Sandwich("Wheat", "Bacon", "", "Lettuce, Tomato");
+            sandwichMenu["PB&J"] = new Sandwich("White", "", "", "Peanut Butter, Jelly");
+            sandwichMenu["Turkey"] = new Sandwich("Rye", "Turkey", "Swiss", "Lettuce, Onion, Tomato");
+
+            sandwichMenu["LoadedBLT"] = new Sandwich("Wheat", "Turkey, Bacon", "American", "Lettuce,Tomato, Onion, Olives");
+            sandwichMenu["ThreeMeatCombo"] = new Sandwich("Rye", "Turkey, Ham, Salami", "Provolone", "Lettuce, Onion");
+            sandwichMenu["Vegetarian"] = new Sandwich("Wheat", "", "", "Lettuce, Onion, Tomato, Olives, Spinach");
+
+            Sandwich sandwichOne = sandwichMenu["BLT"].Clone() as Sandwich;
+            Sandwich sandwichTwo = sandwichMenu["ThreeMeatCombo"].Clone() as Sandwich;
+            Sandwich sandwichThree = sandwichMenu["Vegetarian"].Clone() as Sandwich;
+
+        }
+    }
+}
---
+++ Design Patterns/Prototype/Models/Sandwich.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype.Models
+{
+    public class Sandwich : SandwichPrototype
+    {
+        private string bread;
+        private string meat;
+        private string cheese;
+        private string veggies;
+        public Sandwich(
+            string bread,
+            string meat,
+            string cheese,
+            string veggies
+            ) 
+        {
+            this.bread = bread;
+            this.meat = meat;
+            this.cheese = cheese;
+            this.veggies = veggies;
+        }   
+        public override SandwichPrototype Clone()
+        {
+            string ingredientList = this.GetIngredientList();
+            Console.WriteLine($"Cloning sandwich with ingredients: {ingredientList}");
+            return MemberwiseClone() as SandwichPrototype;
+        }
+
+        public string GetIngredientList() => $"{this.bread}, {this.meat}, {this.cheese}, {this.veggies}";
+    }
+}
