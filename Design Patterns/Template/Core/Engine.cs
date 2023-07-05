76965f0516a6c31a8261fff84cdfd562c43bcdea
Petar Kobakov <pkobakov@gmail.com>
2023-07-05 09:52:24 +0300

    Added New Solution

---
+++ Design Patterns/Template/Core/Engine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template.Core.Contracts;
+using Template.Models;
+
+namespace Template.Core
+{
+    public class Engine : IEngine
+    {
+        public void Run()
+        {
+            Sourdough sourdough = new Sourdough();
+            sourdough.Make();
+
+            TwelveGrain twelveGrain = new TwelveGrain();
+            twelveGrain.Make();
+
+            WholeWheat wholeWheat = new WholeWheat();
+            wholeWheat.Make();
+        }
+    }
+}
