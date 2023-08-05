28f5ca51e88a0c611a47e7ab95c4d8e9df12ebe3
Petar Kobakov <pkobakov@gmail.com>
2023-08-06 00:21:07 +0300

    Added Solution

---
+++ Exam Prep/10 APR 2021/AquaShop/AquaShop/Core/Controller.cs
@@ -0,0 +1,129 @@
+﻿using AquaShop.Core.Contracts;
+using AquaShop.Models.Aquariums;
+using AquaShop.Models.Aquariums.Contracts;
+using AquaShop.Models.Decorations;
+using AquaShop.Models.Decorations.Contracts;
+using AquaShop.Models.Fish;
+using AquaShop.Models.Fish.Contracts;
+using AquaShop.Repositories;
+using AquaShop.Repositories.Contracts;
+using AquaShop.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AquaShop.Core
+{
+    public class Controller : IController
+    {
+        private IRepository<IDecoration> decorations;
+        private List<IAquarium> aquariums;
+        public Controller() 
+        {
+            this.decorations = new DecorationRepository();
+            this.aquariums = new List<IAquarium>();
+
+        }
+        public string AddAquarium(string aquariumType, string aquariumName)
+        {
+            IAquarium aquarium = aquariumType switch
+            {
+                nameof(FreshwaterAquarium) => new FreshwaterAquarium(aquariumName),
+                nameof(SaltwaterAquarium) => new SaltwaterAquarium(aquariumName),
+                _ => throw new InvalidOperationException(ExceptionMessages.InvalidAquariumType)
+
+            };
+
+            aquariums.Add(aquarium);
+            return string.Format(OutputMessages.SuccessfullyAdded, aquariumType);
+        }
+
+        public string AddDecoration(string decorationType)
+        {
+            IDecoration decoration = decorationType switch 
+            {
+                nameof(Plant) => new Plant(),
+                nameof(Ornament) => new Ornament(),
+                _=> throw new InvalidOperationException(ExceptionMessages.InvalidDecorationType)
+            };
+
+            this.decorations.Add(decoration);
+            return string.Format(OutputMessages.SuccessfullyAdded, decorationType);
+        }
+
+        public string AddFish(string aquariumName, string fishType, string fishName, string fishSpecies, decimal price)
+        {
+            IFish fish = fishType switch 
+            {
+                nameof(FreshwaterFish) => new FreshwaterFish(fishName, fishSpecies, price),
+                nameof(SaltwaterFish) => new SaltwaterFish(fishName, fishSpecies, price ),
+                _=> throw new InvalidOperationException(ExceptionMessages.InvalidFishType)
+            };
+
+            IAquarium aquarium = this.aquariums.FirstOrDefault(a => a.Name == aquariumName);
+
+           
+
+            if ((fishType == nameof(FreshwaterFish) && aquarium.GetType().Name != nameof(FreshwaterAquarium)) ||
+                (fishType == nameof(SaltwaterFish) && aquarium.GetType().Name != nameof(SaltwaterAquarium)))
+            {
+                return OutputMessages.UnsuitableWater;
+            }
+
+
+            aquarium.AddFish(fish);
+
+            return string.Format(OutputMessages.EntityAddedToAquarium, fishType, aquariumName);
+        }
+
+        public string CalculateValue(string aquariumName)
+        {
+            IAquarium aquarium = this.aquariums.FirstOrDefault(a => a.Name == aquariumName);
+
+            var fishesPricesSum = aquarium.Fish.Sum( f => f.Price);
+            var decorationPricesSum  = aquarium.Decorations.Sum( d => d.Price);
+            
+            var totalSum = fishesPricesSum + decorationPricesSum;
+
+            return string.Format(OutputMessages.AquariumValue, aquariumName, totalSum);
+        }
+
+        public string FeedFish(string aquariumName)
+        {
+            IAquarium aquarium = this.aquariums.FirstOrDefault(a => a.Name == aquariumName);
+
+            aquarium.Fish.ToList().ForEach(f => f.Eat());
+
+            return string.Format(OutputMessages.FishFed, aquarium.Fish.Count);
+        }
+
+        public string InsertDecoration(string aquariumName, string decorationType)
+        {
+            IDecoration decoration = this.decorations.FindByType(decorationType);
+            IAquarium aquarium = this.aquariums.FirstOrDefault(a => a.Name == aquariumName);
+
+            if (decoration == null) 
+            {
+                throw new InvalidOperationException(string.Format(ExceptionMessages.InexistentDecoration, decorationType));
+            }
+
+            aquarium.AddDecoration(decoration); 
+            this.decorations.Remove(decoration);
+
+            return string.Format(OutputMessages.EntityAddedToAquarium, decorationType, aquariumName);
+        }
+
+        public string Report()
+        {
+           var sb = new StringBuilder();
+
+            foreach (var aquarium in this.aquariums)
+            {
+                sb.AppendLine(aquarium.GetInfo());
+            }
+
+            return sb.ToString().Trim();   
+        }
+    }
+}
