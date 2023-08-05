676ef70b0876fdac614f2321ef9587d2181fb97d
Petar Kobakov <pkobakov@gmail.com>
2023-08-06 00:34:25 +0300

    Added Solution

---
+++ Exam Prep/12 DEC 2020/Bakery/Bakery/Models/BakedFoods/Models/Cake.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bakery.Models.BakedFoods.Models
+{
+    public class Cake : BakedFood
+    {
+        public Cake(string name, decimal price) 
+            : base(name, 245, price)
+        {
+        }
+    }
+}
---
+++ Exam Prep/12 DEC 2020/Bakery/Bakery/Models/Tables/Models/InsideTable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bakery.Models.Tables.Models
+{
+    public class InsideTable : Table
+    {
+        public InsideTable(int tableNumber, int capacity)
+            : base(tableNumber, capacity, 2.50m)
+        {
+        }
+    }
+}
